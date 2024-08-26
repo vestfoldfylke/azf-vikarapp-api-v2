db833778cc9c51748f34a53ae48f32691bc43367
Robin Ellingsen <robin-elli@hotmail.com>
2024-08-26 18:01:02 +0200

    chore: Refactor logToDB.js for improved readability and maintainability

--- src/lib/jobs/logToDB.js
+++ src/lib/jobs/logToDB.js
@@ -8,17 +8,12 @@ const logToDB = async (type = 'info', data, request, context, requestor) => {
     throw new Error('Missing required parameter: data')
   }
   if (Array.isArray(data)) data = data[0]
-
   try {
     // Get the information.
     const sessionId = context?.invocationId || 'unknown'
-    const endpoint = context?.executionContext?.functionName || 'unknown'
-    // Remove request headers and rawBody
-    const apiRequest = { ...request }
-    delete apiRequest.headers
-    delete apiRequest.rawBody
+    const endpoint = context?.functionName || 'unknown'
     const method = request?.method || 'unknown'
-    const origin = request?.headers?.origin || request?.headers?.referer || context?.request?.headers?.origin || context?.request?.headers?.referer || 'unknown'
+    const origin = request?.headers?.get('origin') || 'unknown'
     const url = request?.url || request?.originalUrl || context?.request?.url || context?.request?.originalUrl || 'unknown'
     let startTimeStamp = context?.bindingData?.sys?.utcNow
     const endTimeStamp = new Date()
@@ -27,7 +22,6 @@ const logToDB = async (type = 'info', data, request, context, requestor) => {
       startTimeStamp = Date.parse(startTimeStamp)
       if (startTimeStamp) duration = endTimeStamp - new Date(startTimeStamp)
     }
-
     // Connect to the database
     const mongoClient = await getMongoClient()
     // Create the entry
@@ -35,21 +29,23 @@ const logToDB = async (type = 'info', data, request, context, requestor) => {
       type,
       message: data?.message || '',
       sessionId,
-      origin,
+      origin: origin,
       method,
       endpoint,
       url,
       request,
-      requestor,
+      requestor: {...requestor},
       duration,
       data,
       startTimeStamp,
       endTimeStamp
     }
+    // console.log(logEntry)
     // Save the entry
     await mongoClient.db(mongoDB.DB_NAME).collection(mongoDB.LOG_COLLECTION).insertOne(logEntry)
   } catch (error) {
     // Logger her
+    logger('error', ['logToDB', 'An error occured while trying to log to the database', error?.message || error])
     throw new Error('An error occured while trying to log to the database')
   }
 }
