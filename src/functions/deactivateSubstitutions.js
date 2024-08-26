1ca5eed525c5a98538bcd11f1b80c76e7514fb49
Robin Ellingsen <robin-elli@hotmail.com>
2024-08-26 18:03:22 +0200

    chore: Update deactivateSubstitutions timer schedule

--- src/functions/deactivateSubstitutions.js
+++ src/functions/deactivateSubstitutions.js
@@ -4,14 +4,14 @@ const { deactivateSubstitutions } = require('../lib/jobs/graphJobs');
 const { logToDB } = require('../lib/jobs/logToDB');
 const { logger } = require('@vtfk/logger');
 
-app.timer('activateSubstitutions', {
-    schedule: '0 0 23 * * *',
+app.timer('deactivateSubstitutions', {
+    schedule: '*/15 22-23,0-2 * * *', // At every 15th minute past every hour from 22 through 23 and every hour from 0 through 2.
     handler: async (context) => {
         if(['true', true].includes(config.APP_DEACTIVATE_TIMERS)) return;
         try {
             await deactivateSubstitutions(false, undefined, undefined, context)
         } catch (error) {
-            logger('error', ['activateSubstitutions', 'An error occured while trying to activate substitutions', error?.message || error])
+            logger('error', ['deactivateSubstitutions', 'An error occured while trying to deactivate substitutions', error?.message || error])
             await logToDB('error', error, undefined, context)
         }
     }
